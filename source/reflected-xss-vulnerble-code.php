7a6f998f7f79adde0a0a6b16b9c53c2119a7d7bc
Shuseel Baral <baral.shuseel@gmail.com>
2026-03-06 21:26:55 +0545

    Update reflected-xss-vulnerble-code.php

--- source/reflected-xss-vulnerble-code.php
+++ source/reflected-xss-vulnerble-code.php
@@ -1,18 +1,13 @@
+
 <?php
+
+header ("X-XSS-Protection: 0");
 
 // Is there any input?
 if( array_key_exists( "name", $_GET ) && $_GET[ 'name' ] != NULL ) {
-    // Check Anti-CSRF token
-    checkToken( $_REQUEST[ 'user_token' ], $_SESSION[ 'session_token' ], 'index.php' );
-
-    // Get input
-    $name = htmlspecialchars( $_GET[ 'name' ] );
-
     // Feedback for end user
-    echo "<pre>Hello {$name}</pre>";
+    echo '<pre>Hello ' . $_GET[ 'name' ] . '</pre>';
 }
 
-// Generate Anti-CSRF token
-generateSessionToken();
-
 ?>
+
